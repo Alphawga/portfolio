5eec98195b59a27a0bb8a861a827b90cd179e33f
Bamidele Ajibola <107488257+Alphawga@users.noreply.github.com>
2025-04-19 08:15:27 +0100

    changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
     experience: useRef<HTMLElement>(null),
     projects: useRef<HTMLElement>(null),
     contact: useRef<HTMLElement>(null),
+    
   }
 
   useEffect(() => {
